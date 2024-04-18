176b5e1c533fb23842a9f5f878cc35b8a8eb5331
pks2828 <danieldoriamoncada28@gmail.com>
2024-04-18 10:50:00 -0600

    Antes de insertar lote

--- src/common/pipes/parse-mongo-id/parse-mongo-id.pipe.ts
+++ src/common/pipes/parse-mongo-id/parse-mongo-id.pipe.ts
@@ -5,12 +5,12 @@ import { isValidObjectId } from 'mongoose';
 export class ParseMongoIdPipe implements PipeTransform {
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  transform(value: string, metadata: ArgumentMetadata) {
+  transform(value: string, metadata: ArgumentMetadata) { 
     // console.log({value,metadata});
     
     if( !isValidObjectId(value) ){
       throw new BadRequestException(`"${value}" is not a valid mongo id`);
-    }
+    } 
 
     return value;
   }
